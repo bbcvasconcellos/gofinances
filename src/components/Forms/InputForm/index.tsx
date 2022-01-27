b662e391f305f4a49921a803bf48394ef6d746e9
Bruno Basaglia Cunha Vasconcellos <bbcvasconcellos@gmail.com>
2022-01-27 02:40:09 -0300

    error stylization added

--- src/components/Forms/InputForm/index.tsx
+++ src/components/Forms/InputForm/index.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { Control, Controller } from "react-hook-form";
 import { TextInputProps } from "react-native";
 import { Input } from "../Input";
-import { Container } from "./styles";
+import { Container, Error } from "./styles";
 
 interface InputFormProps extends TextInputProps {
   control: Control;
   name: string;
+  error: string;
 }
 
-export const InputForm = ({ control, name, ...rest}: InputFormProps) => {
+export const InputForm = ({ control, name, error, ...rest}: InputFormProps) => {
   return (
     <Container>
       <Controller
@@ -23,6 +24,7 @@ export const InputForm = ({ control, name, ...rest}: InputFormProps) => {
         )}
         name={name}
       />
+      {error && <Error>{error}</Error>}
     </Container>
   )
 }
